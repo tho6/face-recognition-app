f9b72283cd02bb3860079050773a1e615797c9af
Tavin Ho <chingching6@gmail.com>
2020-10-15 16:11:44 +0800

    Updates

---
+++ precache-manifest.ce639824c2ad3f6c411917dcdd8fd8df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70d44cf4fc5f6ea0ad05d58db5edb9e",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "d0d6d3e6a63b1f671721",
+    "url": "/smartbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "87510cea80bf51a9ad77",
+    "url": "/smartbrain/static/css/main.51d8a26d.chunk.css"
+  },
+  {
+    "revision": "d0d6d3e6a63b1f671721",
+    "url": "/smartbrain/static/js/2.dfd12cd0.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smartbrain/static/js/2.dfd12cd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87510cea80bf51a9ad77",
+    "url": "/smartbrain/static/js/main.ed413145.chunk.js"
+  },
+  {
+    "revision": "8aadafebbf1998937f19",
+    "url": "/smartbrain/static/js/runtime-main.03067a41.js"
+  },
+  {
+    "revision": "d9407b23374f83510327c20b617e8b61",
+    "url": "/smartbrain/static/media/brain.d9407b23.png"
+  }
+]);
